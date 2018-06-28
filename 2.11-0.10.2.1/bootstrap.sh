7d4f3ae46555493f4565b279581d67817d84254a
Zhang Jiajun <kxdmmr@163.com>
2018-06-28 19:49:42 +0800

    [REV] Optimize memory configuration.

--- 2.11-0.10.2.1/bootstrap.sh
+++ 2.11-0.10.2.1/bootstrap.sh
@@ -5,44 +5,45 @@ if [[ x$DEBUG != x ]]; then
 fi
 
 # Set java vm start options
-case ${MEMORY_SIZE} in
+case ${MEMORY_SIZE:-small} in
     "micro")
-       export default_java_mem_opts="-Xmx90m"
+       export default_java_mem_opts="-Xms90m -Xmx90m -Xss512k  -XX:MaxDirectMemorySize=12M"
        echo "Optimizing java process for 128M Memory...." >&2
        ;;
     "small")
-       export default_java_mem_opts="-Xmx180m"
+       export default_java_mem_opts="-Xms180m -Xmx180m -Xss512k -XX:MaxDirectMemorySize=24M "
        echo "Optimizing java process for 256M Memory...." >&2
        ;;
     "medium")
-       export default_java_mem_opts="-Xmx360m"
+       export default_java_mem_opts="-Xms360m -Xmx360m -Xss512k -XX:MaxDirectMemorySize=48M"
        echo "Optimizing java process for 512M Memory...." >&2
        ;;
     "large")
-       export default_java_mem_opts="-Xmx720m"
+       export default_java_mem_opts="-Xms720m -Xmx720m -Xss512k -XX:MaxDirectMemorySize=96M "
        echo "Optimizing java process for 1G Memory...." >&2
        ;;
     "2xlarge")
-       export default_java_mem_opts="-Xmx1420m"
+       export default_java_mem_opts="-Xms1420m -Xmx1420m -Xss512k -XX:MaxDirectMemorySize=192M"
        echo "Optimizing java process for 2G Memory...." >&2
        ;;
     "4xlarge")
-       export default_java_mem_opts="-Xmx2840m"
+       export default_java_mem_opts="-Xms2840m -Xmx2840m -Xss512k -XX:MaxDirectMemorySize=384M "
        echo "Optimizing java process for 4G Memory...." >&2
        ;;
     "8xlarge")
-       export default_java_mem_opts="-Xmx5680m"
+       export default_java_mem_opts="-Xms5680m -Xmx5680m -Xss512k -XX:MaxDirectMemorySize=768M"
        echo "Optimizing java process for 8G Memory...." >&2
        ;;
     16xlarge|32xlarge|64xlarge)
-       export default_java_mem_opts="-Xmx8G"
+       export default_java_mem_opts="-Xms8G -Xmx8G -Xss512k -XX:MaxDirectMemorySize=1536M"
        echo "Optimizing java process for biger Memory...." >&2
        ;;
     *)
-       export default_java_mem_opts="-Xmx128m"
+       export default_java_mem_opts="-Xms128m -Xmx128m -Xss512k -XX:MaxDirectMemorySize=24M"
        echo "Optimizing java process for 256M Memory...." >&2
        ;;
 esac
+
 
 if [[ "${KAFKA_HEAP_OPTS}" == *-Xmx* ]]; then
   export KAFKA_HEAP_OPTS="$KAFKA_HEAP_OPTS -Dlogging.level=$LOGGING_LEVEL -Dfile.encoding=$FILE_ENCODING"
